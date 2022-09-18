b0a22deefb6cd4869d30aa495e6e836215348d98
hurfy <hurfydev@gmail.com>
2022-09-18 21:38:42 +0300

    -f-Minor

--- main.lua
+++ main.lua
@@ -1,24 +1,14 @@
--- Menu
-include("menu/menu.lua")
+main = {}
 
--- Core
-include("Core/coreStats.lua")
+include("loader.lua")
 
--- Data
-include("Data/animsData.lua")
-include("Data/carTuneData.lua")
-include("Data/checkpointsData.lua")
-include("Data/colorsData.lua")
-include("Data/componentsData.lua")
-include("Data/vehiclesData.lua")
-include("Data/weaponsData.lua")
-include("Data/weatherData.lua")
-include("Data/wheelsData.lua")
-
--- Main
 local script = ScriptHook.CurrentScript()
 script.Entities = {}
 
 function script:OnLoad()
 	print("Script tools loaded!")
+	loader.ImportLua(loader._luas)
+
+	-- ScriptHook.RegisterKeyHandler("menu", function()
+	-- end)
 end
