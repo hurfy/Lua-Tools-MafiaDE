0e0b71d70687d750bdec9d5bbf3cf954e3899516
hurfy <hurfydev@gmail.com>
2022-09-19 21:40:39 +0300

    f-Minor

--- Core/coreStats.lua
+++ Core/coreStats.lua
@@ -1,6 +1,6 @@
-module("coreStats", package.seeall)
+coreStats = {}
 
-statsData = {
+coreStats.statsData = {
 	-- Vars
 	actualSpeed = nil,
 	actualEngineRotations = nil,
@@ -17,91 +17,91 @@ statsData = {
 	ifNotInTheVeh = "You are not in the vehicle!",
 }
 
-function checkCarSpeedThread(value)
-	if statsData.threads.carSpeedThread == nil then
-		statsData.threads.carSpeedThread = StartThread(function()
-  			if statsData.getVehicle ~= nil then
+function coreStats.checkCarSpeedThread(value)
+	if coreStats.statsData.threads.carSpeedThread == nil then
+		coreStats.statsData.threads.carSpeedThread = StartThread(function()
+  			if coreStats.statsData.getVehicle ~= nil then
   				while true do
     				if value == "km" then
-    					statsData.actualSpeed = statsData.getVehicle:GetSpeedFloat()
+    					coreStats.statsData.actualSpeed = coreStats.statsData.getVehicle:GetSpeedFloat()
     				elseif value == "mile" then
-    					statsData.actualSpeed = statsData.getVehicle:GetSpeedFloat() / 1.6093
+    					coreStats.statsData.actualSpeed = coreStats.statsData.getVehicle:GetSpeedFloat() / 1.6093
     				end
     				Sleep(1)
     			end
     		else
-    			print(statsData.ifNotInTheVeh)
+    			print(coreStats.statsData.ifNotInTheVeh)
     		end
 		end)
 	end
 end
 
-function checkCarActualEngineRotationsThread()
-	if statsData.threads.carActualEngineRotationsThread == nil then
-		statsData.threads.carActualEngineRotationsThread = StartThread(function()
-  			if statsData.getVehicle ~= nil then
+function coreStats.checkCarActualEngineRotationsThread()
+	if coreStats.statsData.threads.carActualEngineRotationsThread == nil then
+		coreStats.statsData.threads.carActualEngineRotationsThread = StartThread(function()
+  			if coreStats.statsData.getVehicle ~= nil then
   				while true do
-    				statsData.actualEngineRotations = statsData.getVehicle:GetEngineRotations()
+    				coreStats.statsData.actualEngineRotations = coreStats.statsData.getVehicle:GetEngineRotations()
     			Sleep(1)
     			end
     		else
-    			print(statsData.ifNotInTheVeh)
+    			print(coreStats.statsData.ifNotInTheVeh)
     		end
 		end)
 	end
 end
 
-function checkCarActualGearThread()
-	if statsData.threads.carActualGearThread == nil then
-		statsData.threads.carActualGearThread = StartThread(function()
-  			if statsData.getVehicle ~= nil then
+function coreStats.checkCarActualGearThread()
+	if coreStats.statsData.threads.carActualGearThread == nil then
+		coreStats.statsData.threads.carActualGearThread = StartThread(function()
+  			if coreStats.statsData.getVehicle ~= nil then
   				while true do
-    				statsData.actualGear = statsData.getVehicle:GetGear()
+    				coreStats.statsData.actualGear = coreStats.statsData.getVehicle:GetGear()
     			Sleep(1)
     			end
     		else
-    			print(statsData.ifNotInTheVeh)
+    			print(coreStats.statsData.ifNotInTheVeh)
     		end
 		end)
 	end
 end
 
-function checkCarActualFuelThread()
-	if statsData.threads.carActualFuelThread == nil then
-		statsData.threads.carActualFuelThread = StartThread(function()
-  			if statsData.getVehicle ~= nil then
+function coreStats.checkCarActualFuelThread()
+	if coreStats.statsData.threads.carActualFuelThread == nil then
+		coreStats.statsData.threads.carActualFuelThread = StartThread(function()
+  			if coreStats.statsData.getVehicle ~= nil then
   				while true do
-    				statsData.actualFuel = statsData.getVehicle:GetActualFuel()
+    				coreStats.statsData.actualFuel = coreStats.statsData.getVehicle:GetActualFuel()
     			Sleep(1)
     			end
     		else
-    			print(statsData.ifNotInTheVeh)
+    			print(coreStats.statsData.ifNotInTheVeh)
     		end
 		end)
 	end
 end
 
-function checkCarActualDirtyThread()
-	if statsData.threads.carActualDirtyThread == nil then
-		statsData.threads.carActualDirtyThread = StartThread(function()
-  			if statsData.getVehicle ~= nil then
+function coreStats.checkCarActualDirtyThread()
+	if coreStats.statsData.threads.carActualDirtyThread == nil then
+		coreStats.statsData.threads.carActualDirtyThread = StartThread(function()
+  			if coreStats.statsData.getVehicle ~= nil then
   				while true do
-    				statsData.actualDirty = statsData.getVehicle:GetDirty()
+    				coreStats.statsData.actualDirty = coreStats.statsData.getVehicle:GetDirty()
     			Sleep(1)
     			end
     		else
-    			print(statsData.ifNotInTheVeh)
+    			print(coreStats.statsData.ifNotInTheVeh)
     		end
 		end)
 	end
 end
 
-function checkDistanceToPointThread(pnt)
-	if statsData.distanceToPointThread == nil then
-		statsData.distanceToPointThread = StartThread(function()
+function coreStats.checkDistanceToPointThread(pnt)
+	if coreStats.statsData.distanceToPointThread == nil then
+		coreStats.statsData.distanceToPointThread = StartThread(function()
 			while true do
 				distanceToPoint = getp():GetPos():DistanceToPoint(pnt)
-				-- print(distanceToPoint)
+				print(distanceToPoint)
 				Sleep(1)
 			end
 		end)
@@ -110,14 +110,15 @@ end
 
 -----Destroy Threads-------------------------------------------------------------------------------
 
-function killThread(thread)
+function coreStats.killThread(thread)
 	DestroyThread(thread)
 end
 
-function killAllThreads()
-	for i = 1, #statsData.threads do
-		DestroyThread(i)
-		statsData.threads[i] = nil
-		i = i + 1
-	end
+function coreStats.killAllThreads()
+	-- for i = 1, #coreStats.statsData.threads do
+	-- 	DestroyThread(i)
+	-- 	coreStats.statsData.threads[i] = nil
+	-- 	i = i + 1
+	-- end
+	DestroyThread(coreStats.statsData.threads)
 end
