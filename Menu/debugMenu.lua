9093bb1c2f1b9ff42ad759293ed22a7617a090cc
hurfy <hurfydev@gmail.com>
2022-09-19 21:41:38 +0300

    a-PlayerUtils

--- Menu/debugMenu.lua
+++ Menu/debugMenu.lua
@@ -16,18 +16,24 @@ local function vehicleStats()
 	end
 end
 
-local function testFunctions()
+local function firstTestFunctions()
 	-- Print here
+	-- coreStats.checkDistanceToPointThread(Math:newVector(1, 1, 1))
+end
+
+local function secondTestFunction()
+	-- Print here
+	-- coreStats.killThread(coreStats.statsData.distanceToPointThread)
 end
 
 local function DebugMenu()
 	local menu = UI.SimpleMenu()
 	menu:SetTitle("Debug Menu")
 	
-    menu:AddButton("Player Stats")
+    menu:AddButton("Player Stats", playerUtils.Info)
 	menu:AddButton("Vehicle Stats", vehicleUtils.Info)
-	menu:AddButton("Test Function")
-	menu:AddButton("Teleport Menu")
+	menu:AddButton("Test Function #1", firstTestFunctions)
+	menu:AddButton("Test Function #2", secondTestFunction)
 
 	return menu
 end
